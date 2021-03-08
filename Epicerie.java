5d1cbff373803a3872220131a4744ccf524db5fa
Ataf90 <46840320+Ataf90@users.noreply.github.com>
2021-03-08 08:50:51 +0100

    Add files via upload

---
+++ Epicerie.java
@@ -0,0 +1,14 @@
+
+public class Epicerie {
+    static double chiffreAffaires;
+    String nom;
+    
+    Epicerie(){
+    }
+    
+    Epicerie(String nom){
+        Epicerie.chiffreAffaires = 0; 
+        this.nom = nom;
+    }
+    
+}
